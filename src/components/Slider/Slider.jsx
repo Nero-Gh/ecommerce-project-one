b25415048c15092ecb4655fc38b2e4e833a7258d
Bismark Kadogbe <neroaboagye@gmail.com>
2023-02-26 15:52:50 +0000

    Added Virtual section

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
+import "./Slider.css";
 
 // swipper style
 import "swiper/css";
@@ -13,25 +14,33 @@ const Slider = () => {
   return (
     <div className="s-container">
       <Swiper
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+        navigation={true}
+        // loopFillGroupWithBlank={true}
         slidesPerView={3}
         spaceBetween={40}
         slidesPerGroup={1}
         loop={true}
       >
-        {SliderProducts.map((slide) => {
+        {/* {SliderProducts.map((slide, i) => {
           <SwiperSlide>
             <div className="left-s">
               <div className="name">
                 <span>{slide.name}</span>
                 <span>{slide.detail}</span>
               </div>
-              <span>{slide.price}</span>
+              <span>{slide.price} Ghc</span>
               <div>Shop Now</div>
             </div>
 
             <img src={slide.img} alt="" className="img-p" />
           </SwiperSlide>;
-        })}
+        })} */}
+        <SwiperSlide>1</SwiperSlide>
+        <SwiperSlide>2</SwiperSlide>
+        <SwiperSlide>3</SwiperSlide>
+        <SwiperSlide>4</SwiperSlide>
       </Swiper>
     </div>
   );
